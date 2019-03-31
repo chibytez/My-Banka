4c9015415070621165e03cf2bdd8a34399d2b4b3
Chibytez <aniakuchibuike@gmail.com>
2019-03-31 16:49:29 +0100

    adding admin debit credit account

--- server/Routes/adminRoute.js
+++ server/Routes/adminRoute.js
@@ -1,12 +1,14 @@
 import {
     activateDeactivateAccount,
-    deleteBankAccount
+    deleteBankAccount,
+    creditDebitAccount
     } from '../Controllers/adminController';
 
 
 const adminRoute = (app) => {
-    app.patch('/api/v1/account/:id',activateDeactivateAccount);
-    app.delete('/api/v1/accounts/:id',deleteBankAccount)
+    app.patch('/api/v1/account/:id', activateDeactivateAccount);
+    app.delete('/api/v1/accounts/:id', deleteBankAccount);
+    app.post('/api/v1/transactions/:id', creditDebitAccount)
 }
 
 export default adminRoute;
