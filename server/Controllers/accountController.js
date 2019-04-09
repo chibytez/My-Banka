15ff4c9d1a958b926fefd5a81faf4d61832861a9
Chibytez <aniakuchibuike@gmail.com>
2019-04-09 14:19:52 +0100

    Chore Test testing the api endpoints [Finish #165164260]

--- server/Controllers/accountController.js
+++ server/Controllers/accountController.js
@@ -5,7 +5,7 @@ export const createAccount =(req, res) => {
     const account = {
         id: accounts.length + 1,
         firstName:req.body.firstName,
-        lpastName:req.body.lastName,
+        lastName:req.body.lastName,
         accountNumber: req.body.accountNumber,
         email: req.body.email,
         type:req.body.type,
@@ -15,12 +15,3 @@ export const createAccount =(req, res) => {
     res.status(201);
     res.send(account);
 };
-
-export const userGetAccount =(req, res) => {
-    const account = accounts.find((r) => r.accountNumber === parseInt(req.params.accountNumber));
-    if (!account) {
-        return res.status(404).send('The account with the given ID was not found.');
-    }
-    res.send(account);
-};
-
--- server/Routes/accountRoute.js
+++ server/Routes/accountRoute.js
@@ -6,7 +6,6 @@ import {
 
 const accountRoute = (app) => {
     app.post('/api/v1/accounts',createAccount);
-    app.get('/api/v1/accounts/:id', userGetAccount)
 }
 
 export default accountRoute;
