ae0579190e9b9c0f0795eb3a8016a652d83fb74f
Chibytez <aniakuchibuike@gmail.com>
2019-04-21 22:42:12 +0100

    feature (user can view a specific account detail) user can view a specific account detail
    [finish #165486364]

--- server/Controllers/accountController.js
+++ server/Controllers/accountController.js
@@ -74,6 +74,28 @@ class AccountController{
       });
     }
  }
+ static async userViewSpecificAccount(req,res){
+  try {
+      const { accountNumber } = req.params;
+      const accountQuery = 'select id, createdon, accountnumber,email, type, status, balance FROM accounts  WHERE accountnumber = $1';
+      const accounts = await db.query(accountQuery, [accountNumber]);
+      if (accounts.rows.length > 0) {
+          return res.status(200).json({
+            status: 200,
+            data: accounts.rows,
+          });
+        };
+        return res.status(404).json({
+          status: 404,
+          error: 'No accound with the given account number',
+        });
+  } catch (error) {
+      return res.status(500).json({
+          status: 500,
+          error: 'Error detected',
+        });
+  }
+  };
 
 };
 export default AccountController;
